02cbae82dc173ba1ba78f07764910671779c825c
Mubashir Codes <mubashiralone1@gmail.com>
2026-06-19 12:44:17 -0400

    Add files via upload
    Chapter 1 code.

---
+++ print_file.py
@@ -0,0 +1,3 @@
+message = "\n Welcome Mubashir Alone in the most famous and powerful\nprogranung languagr. THis programing language can change the whole\n World. A Most powerful language which is called `Python`."
+
+print("Hello, World!" + message)
